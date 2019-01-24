8cd4ccc21c47f8769d910c6f7345266b0e4e04b7
annier4 <annieta14@gmail.com>
2019-01-24 13:56:39 -0500

    Codigo inicial

---
+++ voix/src/MainApp.java
@@ -0,0 +1,21 @@
+
+import processing.core.PApplet;
+
+public class MainApp extends PApplet {
+	
+	private Logica app;
+	
+	public static void main(String[] args){
+		PApplet.main("Main");
+	}
+	
+	@Override
+	public void settings(){
+		size(500,500);
+	}
+	
+	@Override
+	public void setup(){
+		app = new Logica();
+	}
+}
